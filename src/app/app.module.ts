6fcda75dce7724445b27bf528e521f458a0d16a9
Guilherme Caetano <guilhermecaetano367@gmail.com>
2025-05-09 03:11:04 -0300

    att url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ItemComponent,
-    ProdutoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
