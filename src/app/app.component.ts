6f78937bf3c31344ed5e50017eeeff18452c9344
Guilherme Caetano <guilhermecaetano367@gmail.com>
2025-05-09 02:13:47 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from "../tarefa";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TODOapp';
+  emEdicao = false;
+ @Input() tarefa: Tarefa = new Tarefa("", false);
+ @Output() removeTarefa = new EventEmitter();
+ title = 'TODOapp';
+ arrayDeTarefas: Tarefa[] = [];
+ constructor() {
+ this.READ_tarefas();
+ }
+ CREATE_tarefa(descricaoNovaTarefa: string) {
+ var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+ this.arrayDeTarefas.unshift(novaTarefa);
+ }
+ READ_tarefas() {
+ this.arrayDeTarefas = [
+ new Tarefa("Estudar Frameworks WEB", false),
+ new Tarefa("Comer Pizza", false),
+ new Tarefa("Ajudar meus pais", false)
+ ];
+ }
 }
+
+DELETE_tarefa(tarefaAserRemovida : Tarefa) {
+ this.arrayDeTarefas.splice(this.arrayDeTarefas.indexOf(tarefaAserRemovida), 1);
+}
+
+
---
+++ src/app/produto/produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  standalone: false,
+  templateUrl: './produto.component.html',
+  styleUrl: './produto.component.css'
+})
+export class ProdutoComponent {
+
+}
